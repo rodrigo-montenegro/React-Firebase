e5ce2ed9c3517d13d9f28e6413f9cf2ab921415b
rodrigo-montenegro <rodrigommz90@gmail.com>
2021-03-05 01:13:53 -0300

    Ejemplo de Rutas

---
+++ ejemploroute/src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {
+BrowserRouter as Router,
+Route,
+Link,
+Switch
+} from 'react-router-dom'
+import Usuarios from './components/Usuarios'
+import Usuario from './components/Usuario'
+
+
+function App() {
+  return (
+    <Router>
+      <Link to="/">Usuarios</Link>
+      <Switch>
+        <Route exact path="/">
+          <Usuarios/>
+        </Route>
+        <Route path="/usuario/:id">
+          <Usuario/>
+        </Route>
+      </Switch>
+
+    </Router>
+  );
+}
+
+export default App;
